8f50047a9972b860eba130b44c82b9d54ec643ed
SunkiPang <21400350@handong.edu>
2019-04-13 01:17:49 +0900

    modify sub2

--- main.c
+++ main.c
@@ -16,7 +16,7 @@ void sub2(){
 }
 
 void sub3(){
-	printf("\n-----구구단-----\n");
+	printf("\n---------------구구단---------------\n");
 	for (int i = 1; i < 10; i++){
 		for(int j = 1; j < 10; j++){
 			printf("%d ",i*j);
