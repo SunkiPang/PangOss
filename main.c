f54ecacf6b0d0f01ecd9c335dc783148ea7869bc
SunkiPang <21400350@handong.edu>
2019-04-12 10:58:41 +0900

    첫번째 main.c

---
+++ main.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+
+void sub1(){
+
+}
+
+void sub2(){
+
+}
+
+void sub3(){
+	printf("\nPangPang\n");
+	for (int i = 1; i < 10; i++){
+		for(int j = 1; j < 10; j++){
+			printf("%d ",i*j);
+		}
+		printf("\n");
+	}
+}
+
+
+void sub4(){
+	
+}
+
+int main(){
+	
+	sub1();
+	sub2();
+	sub3();
+	sub4();
+
+
+	return 0;
+}
